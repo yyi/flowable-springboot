45ca41211cca1383fd9dc917102aa679b1119e18
yyi <yyi.mailer@gmail.com>
2020-01-28 01:47:12 +0800

    update

--- src/main/java/com/haiyang/flowablespringboot/controller/ExpenseController.java
+++ src/main/java/com/haiyang/flowablespringboot/controller/ExpenseController.java
@@ -132,7 +132,10 @@ public class ExpenseController {
         BpmnModel bpmnModel = repositoryService.getBpmnModel(pi.getProcessDefinitionId());
         ProcessEngineConfiguration engconf = processEngine.getProcessEngineConfiguration();
         ProcessDiagramGenerator diagramGenerator = engconf.getProcessDiagramGenerator();
-        InputStream in = diagramGenerator.generateDiagram(bpmnModel, "png", activityIds, flows, engconf.getActivityFontName(), engconf.getLabelFontName(), engconf.getAnnotationFontName(), engconf.getClassLoader(), 1.0);
+      //  InputStream in = diagramGenerator.generateDiagram(bpmnModel, "png", activityIds, activityIds, engconf.getActivityFontName(), engconf.getLabelFontName(), engconf.getAnnotationFontName(), engconf.getClassLoader(), 1.0);
+        InputStream in = diagramGenerator.generateDiagram(bpmnModel, "png", activityIds, activityIds, "宋体", "微软雅黑", "黑体", null, 2.0);
+
+
         OutputStream out = null;
         byte[] buf = new byte[1024];
         int legth = 0;
